138ec2c5717c75d11ed39eee65ec49d88d3afaa3
Gavin Spearhead <urd_github@kmachine.nl>
2014-08-13 18:12:00 +0200

    fix typo

--- html/ajax_create_preview.php
+++ html/ajax_create_preview.php
@@ -31,7 +31,7 @@ try {
     $pbin_id = get_post('preview_bin_id');
     $pgroup_id = get_post('preview_group_id');
     $dlid = start_preview($db, $pbin_id, $pgroup_id, $userid);
-    return_result('dlid' => $dlid));
+    return_result(array('dlid' => $dlid)));
 } catch (exception $e) {
     return_result(array('error' => $e->getMessage()));
 }
