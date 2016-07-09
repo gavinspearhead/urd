70d941a0d561d549d2e0c30effbd47829c9731f0
gavinspearhead <gavinspearhead@gmail.com>
2016-07-09 23:57:24 +0200

    update black/whitelist urls for spotnet (2)

--- functions/defaults.php
+++ functions/defaults.php
@@ -275,7 +275,7 @@ function get_default_config()
 ##$prefArray_root['spots_blacklist']          = 'http://jij.haatmij.nl/spotnet/blacklist.txt'; //black list with spotterIds known for spamming etc
 ##    $prefArray_root['spots_whitelist']          = 'http://jij.haatmij.nl/spotnet/whitelist.txt'; //white list with spotterIds known for solid spots
     $prefArray_root['spots_blacklist']          = 'http://jijhaatmij.hopto.me/blacklist.txt'; //black list with spotterIds known for spamming etc
-    $prefArray_root['spots_whitelist']          = 'http://jijhaatmij.hopto.me/whitelist.tx'; //white list with spotterIds known for solid spots
+    $prefArray_root['spots_whitelist']          = 'http://jijhaatmij.hopto.me/whitelist.txt'; //white list with spotterIds known for solid spots
 
     $prefArray_root['settype_' . urd_extsetinfo::SETTYPE_UNKNOWN ]      = '%C %P %n';
     $prefArray_root['settype_' . urd_extsetinfo::SETTYPE_MOVIE ]        = '%C %P %n %(y) %m %a %l %s %[x] %N ';
